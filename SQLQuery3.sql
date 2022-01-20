624abcdb3421427ef62c9b565c570be12c4acf28
spresman <sampresman@gmail.com>
2022-01-20 11:05:15 -0500

    Add code for Venomous-Encounters-and-Population-Density

---
+++ SQLQuery3.sql
@@ -0,0 +1,62 @@
+--Number of instances of venomous animal contact per 100,000 individuals in countries (2017). 
+--Venomous animals include all animals which deliver toxins as venom through a bite or sting.
+
+SELECT *
+FROM PopulationDensityVenom..['incidence-of-venomous-animal-co$']
+WHERE Year = 2017
+ORDER BY 1
+
+--The number of people per km² of land area in countries (2017).
+
+SELECT *
+FROM PopulationDensityVenom..['population-density$']
+WHERE Year = 2017
+ORDER BY 4
+
+--Creating a table for countries and their venomous encounter data.
+
+DROP TABLE IF EXISTS #VenomousIncidence
+CREATE TABLE #VenomousIncidence
+(
+Country nvarchar(255),
+[Log of Venomous Contact Per km²] numeric
+)
+
+--Inserting into Venom table.
+--NOTE: Only data from the year 2017 was used, and data was log-transformed (normalized) for better visualization. 
+
+INSERT INTO #VenomousIncidence
+SELECT Venom.Entity,
+LOG(Venom.[Incidence - Venomous animal contact - Sex: Both - Age: Age-stand]
+*Pop.[Population density (people per sq# km of land area)] / 100000) AS [Log of Venom Contact]
+
+FROM PopulationDensityVenom..['incidence-of-venomous-animal-co$'] Venom
+INNER JOIN PopulationDensityVenom..['population-density$'] Pop ON Venom.Entity= Pop.Entity
+WHERE Venom.Year = 2017 AND Pop.Year = 2017
+ORDER BY 1
+
+--Creating a Population Density Venom view.
+
+CREATE VIEW VenInc AS
+SELECT Venom.Entity,
+LOG(Venom.[Incidence - Venomous animal contact - Sex: Both - Age: Age-stand]
+*Pop.[Population density (people per sq# km of land area)] / 100000) AS [Log of Venomous Contact Per km²]
+
+FROM PopulationDensityVenom..['incidence-of-venomous-animal-co$'] Venom
+INNER JOIN PopulationDensityVenom..['population-density$'] Pop ON Venom.Entity= Pop.Entity
+WHERE Venom.Year = 2017 AND Pop.Year = 2017
+
+
+--Sample select with just Venomous Contact Per km².
+
+--SELECT Venom.Entity,
+--Venom.[Incidence - Venomous animal contact - Sex: Both - Age: Age-stand]
+--*Pop.[Population density (people per sq# km of land area)] / 100000 AS [Venomous Contact Per km²]
+
+--FROM PopulationDensityVenom..['incidence-of-venomous-animal-co$'] Venom
+--INNER JOIN PopulationDensityVenom..['population-density$'] Pop ON Venom.Entity= Pop.Entity
+--WHERE Venom.Year = 2017 AND Pop.Year = 2017
+--ORDER BY 1
+
+SELECT VenInc.Entity, VenInc.[Log of Venomous Contact Per km²]
+FROM VenInc
